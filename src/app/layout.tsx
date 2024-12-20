7a2aa5f33c849b9a0ac534c8834daab1062ce577
ChaeYubin <cyoobin@naver.com>
2024-12-20 22:59:02 +0900

    fix: 기본 inter 폰트 설정 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
 
 export const metadata: Metadata = {
@@ -8,18 +7,13 @@ export const metadata: Metadata = {
   description: "채유빈의 블로그입니다.",
 };
 
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={inter.className}>
+    <html lang="ko">
       <body>
         <section className="mx-auto max-w-2xl px-4 xl:max-w-3xl">
           <NavBar />
