0c04f7324280f8cc1d3e3cef881ece45698e9710
ChaeYubin <cyoobin@naver.com>
2024-12-20 23:24:33 +0900

    style: 포스트 헤더 스타일 수정

--- src/containers/posts/PostHeader.tsx
+++ src/containers/posts/PostHeader.tsx
@@ -1,16 +1,12 @@
-import React from "react";
+import { CalendarDays, Clock3, Eye } from "lucide-react";
+
 import { Post } from "./types";
-import { CalendarDays, Clock3 } from "lucide-react";
-import TagList from "@/components/TagList";
 
 const PostHeader = ({ post }: { post: Post }) => {
   return (
-    <header className="mt-2 text-center">
-      <h1 className="mb-3 text-4xl font-bold">{post.title}</h1>
-      {post.tags && post.tags.length > 0 && (
-        <TagList post={post} isCenter={true} />
-      )}
-      <div className="flex justify-center pt-3 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+    <header className="mt-2 mb-8">
+      <h1 className="mb-3 text-5xl font-extrabold">{post.title}</h1>
+      <div className="flex pt-3 space-x-2 text-sm text-gray-500 dark:text-gray-400">
         <div className="flex items-center space-x-1">
           <CalendarDays className="w-3.5" />
           <span>{post.dateString}</span>
@@ -18,6 +14,10 @@ const PostHeader = ({ post }: { post: Post }) => {
         <div className="flex items-center space-x-1">
           <Clock3 className="w-3.5" />
           <span>3분</span>
+        </div>
+        <div className="flex items-center space-x-1">
+          <Eye className="w-3.5" />
+          <span>1,034</span>
         </div>
       </div>
       <hr className="mt-5 mb-10" />
