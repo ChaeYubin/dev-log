a07aeb2f77f70d277385c0a23b2a09d0ae1803cf
ChaeYubin <cyoobin@naver.com>
2024-12-26 19:38:23 +0900

    feat: 이벤트 루프 글 업로드

--- src/containers/mdx/Image.tsx
+++ src/containers/mdx/Image.tsx
@@ -8,7 +8,7 @@ const Image = ({ src, alt }: ImageProps) => {
     <>
       <img src={src} alt={alt} className="mx-auto mb-0 mt-8 rounded-md" />
       {alt !== "" && (
-        <span className="mb-8 mt-2 block w-full text-center text-sm text-gray-500 dark:text-gray-400">
+        <span className="mb-8 mt-2 block w-full text-center text-sm text-gray-500 dark:text-gray-300">
           {alt}
         </span>
       )}
